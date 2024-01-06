ed91be7a61e7f0cdf165bb0c949714834299ff6b
Júlia Nascimento <juliavcostan@gmail.com>
2024-01-06 19:47:57 -0300

    Ponto de partida para um novo projeto. Criação da tela de boas vindas com cronometro para passar para outra tela

---
+++ android/app/src/main/kotlin/com/example/love_is_love/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.love_is_love
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/welcome_screen.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'package:love_is_love/pages/main_screen.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Future.delayed(
+      Duration(seconds: 5),
+      () {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (c) {
+              return MainScreen();
+            },
+          ),
+        );
+      },
+    );
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: 300,
+              child: Lottie.asset(
+                  'assets/animations_welcome_screen/animation_coracao_veloz.json'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
