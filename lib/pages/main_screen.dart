fe2c3613b521024cf0efe3f182acd7808e767a8f
Júlia Nascimento <juliavcostan@gmail.com>
2024-01-10 13:46:24 -0300

    Back Ground Image na tela inicial

--- lib/pages/main_screen.dart
+++ lib/pages/main_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
 import 'package:love_is_love/components/button_component.dart';
@@ -14,36 +16,30 @@ class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              'OLÁ JÚLIA, SEJA BEM VINDA',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 20,
-                color: Colors.purple,
-              ),
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/image/julia_photo.jpeg'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: BackdropFilter(
+          filter: ImageFilter.blur(sigmaX: 2.0, sigmaY: 2.0),
+          child: const Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                 Padding(
+                  padding: EdgeInsets.fromLTRB(80, 70, 80, 0),
+                  child: ButtonComponent(
+                    texto: 'VAMOS INICIAR',
+                    alignment: Alignment.center,
+                    tela: StartScreen(),
+                  ),
+                ),
+              ],
             ),
-            SizedBox(height: 10),
-            SizedBox(
-              height: 100,
-              child: Lottie.asset(
-                'assets/animations_welcome_screen/animation_batida_coracao.json',
-                fit: BoxFit.cover,
-              ),
-            ),
-           
-           const Padding(
-              padding:  EdgeInsets.fromLTRB(80, 70, 80, 0),
-              child:  ButtonComponent(
-                texto: 'VAMOS INICIAR',
-                alignment: Alignment.center,
-                tela: StartScreen(),
-              ),
-            ),
-          ],
+          ),
         ),
       ),
     );
